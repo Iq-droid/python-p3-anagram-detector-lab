f79770a3c1d8cd9e06c0a4c2fcaa1f8da1c95188
Iqra <iqra.ali@student.moringaschool.com>
2023-09-18 09:51:41 +0300

    anagram

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,21 @@
 # your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word
+        
+    def match(self, word_list):
+        #list to store matching anagrams
+        matching_anagrams = []
+        #converting word to lower case and sorting x-ters
+        sorted_word = sorted(self.word.lower())
+        #Iterate through each word in the list
+        for candidate in word_list:
+            #convert the candidate word to lowercase + sorting x-ters
+            sorted_candidate = sorted(candidate.lower())
+            
+            #checking if words are equal and not identical
+            if sorted_word == sorted_candidate and self.word.lower() != candidate.lower():
+                matching_anagrams.append(candidate)
+        
+        return matching_anagrams
+        
